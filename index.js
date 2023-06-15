eae5d856394d152ad5b8f4bd346abc67a4530d0d
Mikael Moura <mikael.moura79@gmail.com>
2023-06-15 20:55:34 -0300

    retrieves all data and validates the elements

--- index.js
+++ index.js
@@ -15,13 +15,47 @@ async function scraperDiaudi() {
     const html = await page.content();
     const $ = cheerio.load(html);
 
-    $('body > div.container-padrao.reserva > div > div > div > div.content-reserva > div > div.col-12.col-lg-9.pr-lg-0 > div:nth-child(3) > div > div').each((i,e) => {
-        const nameSuite = $(e).find('div > form > div.row > div.col-12.col-lg-7 > div.row.head-apto > div > span').text()
-        console.log(nameSuite);
-        const totalPrice = $(e).find('div > form > div.info-reserva-quarto > div.tarifas > div:nth-child(2) > div > div.col-6.col-sm-6 > span').text()
+    $('body > div.container-padrao.reserva > div > div > div > div.content-reserva > div > div.col-12.col-lg-9.pr-lg-0 > div:nth-child(3) > div > div').each((i,room) => {
+        let nameRooms = $(room).find('div > form > div.row > div.col-12.col-lg-7 > div.row.head-apto > div > span');
+        let nameRoom = nameRooms? nameRooms.text() : '';
+        console.log(nameRoom);
+
+        let totalPrices = $(room).find('div > form > div.info-reserva-quarto > div.tarifas > div:nth-child(2) > div > div.col-6.col-sm-6 > span');
+        let totalPrice = totalPrices? totalPrices.text(): '';
         console.log(totalPrice);
-        const dailyPrice = $(e).find('div > form > div.info-reserva-quarto > div.tarifas > div:nth-child(2) > div > div.col-6.col-sm-3 > span > span.valor-sem-desconto > span').text()
+
+        let dailyPrices = $(room).find('div > form > div.info-reserva-quarto > div.tarifas > div:nth-child(2) > div > div.col-6.col-sm-3 > span > span.valor-sem-desconto > span');
+        let dailyPrice = dailyPrices? dailyPrices.text(): '';
         console.log(dailyPrice);
+
+        const setOfAmenities = [];
+
+        let amenities = $(room).find('div > form > div.row > div.col-12.col-lg-7 > div.row.caracteristicas-apto.align-items-center > div');
+        if(amenities){
+            amenities.each((i,convenience) =>{
+                const amenity = $(convenience).attr('title')
+                setOfAmenities.push(amenity);
+            })
+        }else {
+            amenities = '';
+        }
+        console.log(setOfAmenities)
+
+        let imgMain = $(room).find('div > form > div.row > div.col-12.col-lg-5.pr-lg-0 > div > div > div.owl-stage-outer > div > div.owl-item.active > div > a');
+        let photoMain = imgMain ? imgMain.attr('href'): '';
+        console.log(photoMain)
+
+        const allImages = [];
+        let images = $(room).find('div > form > div.row > div.col-12.col-lg-5.pr-lg-0 > div > div > div.owl-stage-outer > div > div.owl-item > div > a');
+        if(images){
+            images.each((i, imgs) =>{
+                const photos = $(imgs).attr('href');
+                allImages.push(photos)
+            });
+        }else{
+            images = '';
+        }
+        console.log(allImages)     
     });
     
 
